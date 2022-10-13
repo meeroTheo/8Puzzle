99196c24ee670867df7c1b21d7abbc9ab7c98404
meeroTheo <meerbalochandhammy12@gmail.com>
2022-10-13 15:22:31 -0400

    fix: add DP function and solvable function

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
-import time
 
 def main():
     """
     Main function
 
     """
+    # Determines usable puzzle size by user input
+    puzzleSize = 0
+    puzzleSizeList = [8, 15, 24]
+    while puzzleSize not in puzzleSizeList:
+        puzzleSize = int(
+            input("Enter puzzle size (8-puzzle, 15 puzzle, 24 puzzle): "))
 
-    
+
+def disorder(puzzle):
+    """
+    Determines the disorder parameter
+    """
+    size = len(puzzle)-1
+    disorder_num = 0
+
+    for i in range(size):
+        for j in range(1+i, size):
+            if (puzzle[j][i] > 0) and puzzle[j][i] > puzzle[i][j]:
+                disorder += 1
+    return disorder
 
 
+def isSolvable(puzzle):
+    """
+    Determines if the puzzle is solvable (DP even)
+    """
+    disorder = disorder(puzzle)
+    return (disorder % 2 == 0)
--- puzzle.py
+++ puzzle.py
@@ -3,3 +3,14 @@ class Puzzle:
     def __init__(self, board, size):
         self.board = board
         self.size = size
+
+    def puzzleEndState(puzzle_size):
+        if puzzle_size == 8:
+            puzzleEndState = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
+        elif puzzle_size == 15:
+            puzzleEndState = [[1, 2, 3, 4], [5, 6, 7, 8],
+                              [9, 10, 11, 12], [13, 14, 15, 0]]
+        else:  # 24 puzzle
+            puzzleEndState = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [
+                11, 12, 13, 14, 15], [16, 17, 18, 19, 20], [21, 22, 23, 24, 0]]
+        return puzzleEndState
