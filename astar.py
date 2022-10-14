9d3ba1f74baf0177c7316a1ce0729f0e5420124e
meeroTheo <meerbalochandhammy12@gmail.com>
2022-10-14 17:40:29 -0400

    fix: add comments to solve astar

--- astar.py
+++ astar.py
@@ -77,13 +77,14 @@ class Astar:
         """
         Gets the state of an adjacent node 
         which we swap with the 0
+        Returns the new state
         """
-        # check if out of bounds
+        # check if the move is valid
         if (not ((x2 < self.puzzle.size and x2 >= 0)
                  and (y2 < self.puzzle.size and y2 >= 0))):
             return None
 
-        # swap
+        # swap the 0 with the adjacent node
         tempState = self.deepcopy(self.puzzle)
         tempVal = tempState.board[x2][y2]
         tempState.board[x2][y2] = tempState.board[x1][y1]
@@ -93,27 +94,27 @@ class Astar:
         return tempState
 
     def solve(self):
-
+        """
+        Solve the 8 puzzle using A* Search
+        """
         initialState = self.puzzle.board
         goalState = self.puzzle.puzzleEndState()
         start = Puzzle(initialState, self.puzzle.size, 0, 0, 0, 0)
-        goal = Puzzle(goalState, self.puzzle.size, 0, 0, 0, 0)
-
         startNode = Astar(start)
-        endNode = Astar(goal)
-        g = self.puzzle.g
-
-        x, y = self.findzero()
-        coords = [[x, y-1], [x, y+1], [x-1, y], [x+1, y]]
 
         visit1 = deque(startNode)
         visit2 = deque(startNode)
         visit3 = deque(startNode)
 
+        x, y = self.findzero()
+        coords = [[x, y-1], [x, y+1], [x-1, y], [x+1, y]]  # up down left right
+        # put in while loop
         for i in coords:
-
+            # get the state of the adjacent node
             path = self.moves(x, y, i[0], i[1])
+
             if path:
+                # create a new node with the state of the adjacent node
                 pathz = Astar(path)
                 # calculate heuristic function(comment)
                 pathz.f1()
@@ -122,7 +123,7 @@ class Astar:
                 visit1.add(pathz)
                 visit2.add(pathz)
                 visit3.add(pathz)
-
+        # sort to find the node with the lowest f value which represents the best path
         visit1 = deque(sorted(list(visit1), key=lambda puzzle: puzzle.f1))
         visit2 = deque(sorted(list(visit2), key=lambda puzzle: puzzle.f2))
         visit3 = deque(sorted(list(visit3), key=lambda puzzle: puzzle.f3))
