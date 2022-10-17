e56cea964f2a851b0f5cedcec0a8daa6316fceb0
Frost <frostedwolf789@gmail.com>
2022-10-17 16:05:11 -0400

    loop for 34 states, record h1-h3 in list

--- astar.py
+++ astar.py
@@ -1,7 +1,7 @@
 
 from queue import PriorityQueue
 from puzzle import Puzzle
-
+import time
 from collections import deque
 
 
@@ -44,9 +44,119 @@ class Astar:
                 if (puzzle.board[i][j] == 0):
                     return i, j
 
-    def solve(self):
+    def solve1(self):
         pqueue = []
         seen = set()
+        nodecount = 0
+        g = 0
+        pqueue.append((self.f1, self))
+        seen.add(str(self.puzzle))
+        count = 0
+        while True:
+            # evaluates node in priority queue with smallest f case
+            node = pqueue.pop()[1]
+            g = node.g + 1
+            x, y = self.findzero(node.puzzle)
+            coords = [[x, y-1], [x, y+1], [x-1, y], [x+1, y]]
+            for i in coords:
+                new_puzzle = Puzzle.moves(node.puzzle, x, y, i[0], i[1])
+                if new_puzzle:
+                    new_puzzle.set_h3()
+                    if new_puzzle.h3 == 0:
+                        child = Astar(new_puzzle, g)
+                        child.parent = node
+                        node = child
+                        break
+                    if (str(new_puzzle) not in seen):
+                        nodecount+=1
+                        seen.add(str(new_puzzle))
+                        child = Astar(new_puzzle, g)  # create child
+                        child.parent = node
+                        child.set_f1
+                        pqueue.append((child.f1, child))  # add to queue
+                        # sort queue
+                        pqueue.sort(key=lambda x: x[0], reverse=True)
+            # print(node.puzzle)
+            #print(node.g)
+            count += 1
+            # if (count == 100):
+            #    break
+            if (node.puzzle.h3 == 0):
+                route = []
+                #print("\n")
+                #print("ROUTE:\n")
+                steps = 0
+                while node is not None:
+                    route.append(node)
+                    node = node.parent
+                    steps += 1
+                #route.reverse()
+                #print("Number of steps: ")
+                #print(steps)
+                #for state in route:
+                    #print(state.puzzle)
+                break
+        return steps, nodecount
+
+    def solve2(self):
+        pqueue = []
+        seen = set()
+        nodecount = 0
+        g = 0
+        pqueue.append((self.f2, self))
+        seen.add(str(self.puzzle))
+        count = 0
+        while True:
+            # evaluates node in priority queue with smallest f case
+            node = pqueue.pop()[1]
+            g = node.g + 1
+            x, y = self.findzero(node.puzzle)
+            coords = [[x, y-1], [x, y+1], [x-1, y], [x+1, y]]
+            for i in coords:
+                new_puzzle = Puzzle.moves(node.puzzle, x, y, i[0], i[1])
+                if new_puzzle:
+                    new_puzzle.set_h1()
+                    if new_puzzle.h1 == 0:
+                        child = Astar(new_puzzle, g)
+                        child.parent = node
+                        node = child
+                        break
+                    if (str(new_puzzle) not in seen):
+                        nodecount+=1
+                        seen.add(str(new_puzzle))
+                        child = Astar(new_puzzle, g)  # create child
+                        child.parent = node
+                        child.set_f2
+                        pqueue.append((child.f2, child))  # add to queue
+                        # sort queue
+                        pqueue.sort(key=lambda x: x[0], reverse=True)
+            # print(node.puzzle)
+            #print(node.g)
+            count += 1
+            # if (count == 100):
+            #    break
+            if (node.puzzle.h3 == 0):
+                route = []
+                #print("\n")
+                #print("ROUTE:\n")
+                steps = 0
+                while node is not None:
+                    route.append(node)
+                    node = node.parent
+                    steps += 1
+                #route.reverse()
+                #print("Number of steps: ")
+                #print(steps)
+                #for state in route:
+                    #print(state.puzzle)
+                break
+        return steps, nodecount
+
+    def solve3(self):
+        start_time=time.time()
+        pqueue = []
+        seen = set()
+        nodecount= 0
         g = 0
         pqueue.append((self.f3, self))
         seen.add(str(self.puzzle))
@@ -67,6 +177,7 @@ class Astar:
                         node = child
                         break
                     if (str(new_puzzle) not in seen):
+                        nodecount+=1
                         seen.add(str(new_puzzle))
                         child = Astar(new_puzzle, g)  # create child
                         child.parent = node
@@ -75,22 +186,26 @@ class Astar:
                         # sort queue
                         pqueue.sort(key=lambda x: x[0], reverse=True)
             # print(node.puzzle)
-            print(node.g)
+            #print(node.g)
             count += 1
             # if (count == 100):
             #    break
+            
             if (node.puzzle.h3 == 0):
                 route = []
-                print("\n")
-                print("ROUTE:\n")
+                #print("\n")
+                #print("ROUTE:\n")
                 steps = 0
                 while node is not None:
                     route.append(node)
                     node = node.parent
                     steps += 1
-                route.reverse()
-                print("Number of steps: ")
-                print(steps)
-                for state in route:
-                    print(state.puzzle)
+                #route.reverse()
+                #print("Number of steps: ")
+                #print(steps)
+                #for state in route:
+                    #print(state.puzzle)
                 break
+            if((time.time()-start_time) > 15):
+                return g,nodecount,True
+        return steps, nodecount, False
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from puzzle import Puzzle
 from astar import Astar
-import time
+
 
 
 def main():
@@ -15,18 +15,40 @@ def main():
         puzzleSize = int(
             input("Enter puzzle size (8-puzzle, 15 puzzle, 24 puzzle): "))
 
-    start_time=time.time()
-    puzzle1 = Puzzle(puzzleSize)
-    #puzzle2 = Puzzle(puzzleSize)
-
-    #puzzle2.board = [[1, 2, 3], [0, 4, 6], [7, 5, 8]]
-    #puzzle2.setBoard
-
-    print(puzzle1)
-
-    a = Astar(puzzle1, 0)
-    node = a.solve()
-    print("Output:")
-    print(time.time()-start_time)
+    result_steps3=[]
+    result_nodecount3=[]
+    result_steps2=[]
+    result_nodecount2=[]
+    result_steps1=[]
+    result_nodecount1=[]
+    for x in range(34):
+        print("{}%".format(x/34))
+        timeOut=False
+        puzzle = Puzzle(puzzleSize)
+        a = Astar(puzzle, 0)
+        steps, nc, timeOut = a.solve3()
+        if (not timeOut):
+            result_steps3.append(steps)
+            result_nodecount3.append(nc)
+            print("done h3")
+        else:
+            x-=1
+            continue
+        print("---------------------")
+        print(puzzle)
+        print("---------------------")
+        steps, nc= a.solve2()
+        result_steps2.append(steps)
+        result_nodecount2.append(nc)
+        steps, nc = a.solve1()
+        result_steps1.append(steps)
+        result_nodecount1.append(nc)
+    
+    print(result_steps3)
+    print(result_nodecount3)
+    print(result_steps2)
+    print(result_nodecount2)
+    print(result_steps1)
+    print(result_nodecount1)
 
 main()
