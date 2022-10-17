4415efaf57a02e5cdf55143498696767efc099b4
meeroTheo <meerbalochandhammy12@gmail.com>
2022-10-17 13:57:05 -0400

    fix: make solve more efficient

--- astar.py
+++ astar.py
@@ -10,6 +10,7 @@ class Astar:
     def __init__(self, puzzle, g):
         self.puzzle = puzzle
         self.explored = False
+        self.parent = None
         self.g = g
         self.f1 = self.puzzle.h1 + self.g
         self.f2 = self.puzzle.h2 + self.g
@@ -26,8 +27,6 @@ class Astar:
         self.f3 = self.puzzle.h3 + self.g
         return
 
-
-
     def findzero(self, puzzle):
         for i in range(puzzle.size):
             for j in range(puzzle.size):
@@ -35,14 +34,13 @@ class Astar:
                     return i, j
 
     def solve(self):
-        seen = []
         pqueue = []
         g = 0
-        goalstate = Puzzle(self.puzzle.size)
-        Puzzle.setBoard(goalstate, Puzzle.puzzleEndState(goalstate))
+        seen = set()
         parent = self
-        seen.append(parent.puzzle)
         pqueue.append((parent.f2, parent))
+        seen.add(parent.puzzle)
+        count = 0
         while True:
             # evaluates node in priority queue with smallest f case
             node = pqueue.pop()[1]
@@ -52,27 +50,37 @@ class Astar:
             for i in coords:
                 new_puzzle = Puzzle.moves(node.puzzle, x, y, i[0], i[1])
                 if new_puzzle is not None:
-                    Puzzle.setBoard(new_puzzle, new_puzzle.board)
-                    if Puzzle.isEqual(new_puzzle, goalstate):
-                        print(new_puzzle)
-                        return new_puzzle
-                    isSeen = False
-                    j = 0
-                    # checks if path has been seen before
-                    while j < len(seen):
-                        if Puzzle.isEqual(new_puzzle, seen[j]):
-                            isSeen = True
-                            break
-                        j += 1
-                    # puts unseen states into priority queue
-                    if not isSeen:
-                        seen.append(new_puzzle)  # add to seen
+                    if new_puzzle.h2 == 0:
+                        child = Astar(new_puzzle, g)
+                        child.parent = node
+                        node = child
+                        break
+                    if new_puzzle not in seen:
+                        seen.add(new_puzzle)
                         child = Astar(new_puzzle, g)  # create child
+                        child.parent = node
                         pqueue.append((child.f2, child))  # add to queue
                         # sort queue
                         pqueue.sort(key=lambda x: x[0], reverse=True)
-            print(node.puzzle)
+            # print(node.puzzle)
+            print(node.puzzle.h2)
             print(node.f2)
-            if (node.puzzle.h1 == 0):
+            print(node.g)
+            count += 1
+            # if (count == 100):
+            #    break
+            if (node.puzzle.h2 == 0):
+                route = []
+                print("\n")
+                print("ROUTE:\n")
+                steps = 0
+                while node is not None:
+                    route.append(node)
+                    node = node.parent
+                    steps += 1
+                route.reverse()
+                print("Number of steps: ")
+                print(steps)
+                for state in route:
+                    print(state.puzzle)
                 break
-        return node.puzzle
